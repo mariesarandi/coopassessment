d20c0cf8db808124576d6ca22187c0c0d87b5a45
maybetracie <maybetracie@DESKTOP-4KSQEVD>
2021-02-10 23:19:39 -0500

    Service added to angular app

---
+++ Angular/ProductDashBoard/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ProductService } from './product.service'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
